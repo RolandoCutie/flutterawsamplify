755013d6c967806d312e3812b819767a7356149c
Rolando Garcia Cutie <rolando.garcia@generalsoftwareinc.com>
2023-03-08 15:51:11 -0500

    first_commit

---
+++ android/app/src/main/java/com/example/awsamplify/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.awsamplify;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/presentation/pages/todos_page/todos_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TodosView extends StatelessWidget {
+  const TodosView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
